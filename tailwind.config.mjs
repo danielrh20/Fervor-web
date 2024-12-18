aa0b69a48e89973da8dd3eb6ca9e947eca58a819
Daniel <danielriverah2003@gmail.com>
2024-12-18 04:57:04 -0500

    Actualización de contador

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,14 +2,13 @@ import animations from '@midudev/tailwind-animations'
 
 export default {
   content: [
-    './src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx,vue,svelte}',
-    './src/assets/css/main.css',
+    './src/**/*.{astro,html,js,jsx,ts,tsx,vue,svelte}',
   ],
   theme: {
     extend: {
       colors: {
         primary: '#5d35f5',
-        secundary: '#ffffff',
+        secondary: '#ffffff',
       },
     },
   },
